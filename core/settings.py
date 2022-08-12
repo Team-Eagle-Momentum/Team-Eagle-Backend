584d3ce764bf4faa7604449d1007d6470d553a2e
Metta Rolando <mnrolando3@gmail.com>
2022-08-12 13:34:47 -0400

    Added staticfilesstorage in settings

--- core/settings.py
+++ core/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Default primary key field type
